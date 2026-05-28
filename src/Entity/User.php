4c7d8f7579b4a1dcb4e973f90ef6540af8ae1539
Modlem <modlem@example.com>
2026-05-28 23:21:16 +0200

    user entity

---
+++ src/Entity/User.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace App\Entity;
+
+use App\Enum\Roles;
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $firstname = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $lastname = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $password = null;
+
+    #[ORM\Column]
+    private ?bool $is_verified = null;
+
+    #[ORM\Column(enumType: Roles::class)]
+    private ?Roles $roles = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $dtype = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $profile_picture = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $otp_code = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTime $otp_expires_at = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): static
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): static
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function isVerified(): ?bool
+    {
+        return $this->is_verified;
+    }
+
+    public function setIsVerified(bool $is_verified): static
+    {
+        $this->is_verified = $is_verified;
+
+        return $this;
+    }
+
+    public function getRoles(): ?Roles
+    {
+        return $this->roles;
+    }
+
+    public function setRoles(Roles $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    public function getDtype(): ?string
+    {
+        return $this->dtype;
+    }
+
+    public function setDtype(string $dtype): static
+    {
+        $this->dtype = $dtype;
+
+        return $this;
+    }
+
+    public function getProfilePicture(): ?string
+    {
+        return $this->profile_picture;
+    }
+
+    public function setProfilePicture(?string $profile_picture): static
+    {
+        $this->profile_picture = $profile_picture;
+
+        return $this;
+    }
+
+    public function getOtpCode(): ?int
+    {
+        return $this->otp_code;
+    }
+
+    public function setOtpCode(?int $otp_code): static
+    {
+        $this->otp_code = $otp_code;
+
+        return $this;
+    }
+
+    public function getOtpExpiresAt(): ?\DateTime
+    {
+        return $this->otp_expires_at;
+    }
+
+    public function setOtpExpiresAt(?\DateTime $otp_expires_at): static
+    {
+        $this->otp_expires_at = $otp_expires_at;
+
+        return $this;
+    }
+}
---
+++ src/Enum/Roles.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enum;
+
+enum Roles: string
+{
+    case ROLE_USER = 'ROLE_USER';
+    case ROLE_ADMIN = 'ROLE_ADMIN';
+}

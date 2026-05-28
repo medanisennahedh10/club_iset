06739a1853bb4e665331f87404745408d21295f6
Modlem <modlem@example.com>
2026-05-28 23:31:08 +0200

    user roles

--- src/Enum/Roles.php
+++ src/Enum/Roles.php
@@ -4,6 +4,9 @@ namespace App\Enum;
 
 enum Roles: string
 {
-    case ROLE_USER = 'ROLE_USER';
+    case ROLE_USER = 'ROLE_ETUDIANT';
     case ROLE_ADMIN = 'ROLE_ADMIN';
+    case ROLE_RESPONSABLE = 'ROLE_RESPONSABLE';
+    case ROLE_PRESIDENT = 'ROLE_PRESIDENT';
 }
+
